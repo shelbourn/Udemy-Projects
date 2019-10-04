b0d5b4c0dc42d3a370b5d17fe894ff1a90cd53af
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-03 22:59:37 -0700

    Type coercion.

--- Javascript/myFiles/script.js
+++ Javascript/myFiles/script.js
@@ -1,27 +1,50 @@
 /**************************
-* Variables and Data Types
-***************************/
+ * Variables and Data Types
+ ***************************/
 
-console.log('Hello World!!!')
+// console.log('Hello World!!!')
 
-var firstName = 'John'
-console.log(firstName)
+// var firstName = 'John'
+// console.log(firstName)
 
-var lastName = 'Smith'
-var age = 28
+// var lastName = 'Smith'
+// var age = 28
 
-var fullAge = true
-console.log(fullAge)
+// var fullAge = true
+// console.log(fullAge)
 
-var job = console.log(job)
+// var job = console.log(job)
 
-job = 'teacher'
-console.log(job)
+// job = 'teacher'
+// console.log(job)
 
-var 3years = 3 // variables can only start with letter, $, or _
+// var 3years = 3 // variables can only start with letter, $, or _
 // variables cannot contain symbols in the middle of variable name
 // variables cannot be named after reserved JS keywords
 
-/************************************ 
-* Variable mutation and type coercion
-*************************************/
+/************************************
+ * Variable mutation and type coercion
+ *************************************/
+
+// Type Coercion
+var firstName = 'John'
+var age = 28
+
+console.log(firstName + ' ' + age) //string joining (type coercion)
+
+var job, isMarried
+job = 'Teacher'
+isMarried = false
+
+console.log(
+	firstName +
+		' ' +
+		age +
+		' ' +
+		'year old ' +
+		job +
+		'. Is he married? ' +
+		isMarried
+)
+
+// Variable Mutation
