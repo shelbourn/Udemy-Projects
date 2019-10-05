7d3f98a74ac83bfb70ea76cf21f2e6c52f5f2cb0
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-04 22:33:31 -0700

    Multiple operators, grouping, multiple assignments

--- Javascript/myFiles/script.js
+++ Javascript/myFiles/script.js
@@ -99,3 +99,21 @@
  * Operator Precedence
  *************************************/
 
+var now = 2018
+var yearJohn = 1989
+var fullAge = 18
+
+// Multiple Operators
+var isFullAge = now - yearJohn >= fullAge
+console.log(isFullAge)
+
+// Grouping
+var ageJohn = now - yearJohn
+var ageMark = 35
+var average = (ageJohn + ageMark) / 2
+console.log(average)
+
+// Multiple Assignments
+var x, y
+x = y = (3 + 5) * 4 - 6 // 8 * 4 - 6 // 32 - 6 // 26
+console.log(x, y)
