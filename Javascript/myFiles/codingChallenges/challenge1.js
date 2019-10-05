c4ff5acf49c5c8f4cbdb6a50ca278d4a7cd5fc21
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-04 23:29:05 -0700

    Editing BMI calculator

--- Javascript/myFiles/codingChallenges/challenge1.js
+++ Javascript/myFiles/codingChallenges/challenge1.js
@@ -2,23 +2,27 @@
  * Coding Challenge 1 - BMI
  */
 
-//  var markWeight, markHeight
-//  markWeight = 200
-//  markHeight = 72
+ var markWeight, markHeight
+ markWeight = 78
+ markHeight = 6
 
-//  var johnWeight, johnHeight
-//  johnWeight = 250
-//  johnHeight = 76
+ var johnWeight, johnHeight
+ johnWeight = 84
+ johnHeight = 5
 
-//  var markBmi = markWeight / markHeight^2
+ var markBmi = markWeight / (markHeight^2)
 
-//  var johnBmi = johnWeight /johnHeight^2
+ var johnBmi = johnWeight /(johnHeight^2)
 
-//  var markIsFatter = markBmi > johnBmi
+ var markIsFatter = markBmi > johnBmi
 
-//  console.log("Is Mark's BMI greater than John's?" + ' ' + markIsFatter)
+ console.log("Is Mark's BMI greater than John's?" + ' ' + markIsFatter)
+ console.log(markBmi)
+ console.log(johnBmi)
 
-var yourWeight = prompt('What is your weight?')
-var yourHeight = prompt('What is your height?')
 
-var yourBmi = alert('Your BMI is ' + yourWeight / yourHeight ^ 2)
+// User input BMI Calculator
+// var yourWeight = prompt('What is your weight?')
+// var yourHeight = prompt('What is your height?')
+
+// var yourBmi = alert('Your BMI is ' + yourWeight / yourHeight ^ 2)
