626cf946dee6a6364c28b4248b267c04c334e078
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-04 23:01:18 -0700

    Coding challenge 1.

---
+++ Javascript/myFiles/codingChallenges/challenge1.js
@@ -0,0 +1,19 @@
+/***********************
+ * Coding Challenge 1 - BMI
+ */
+
+ var markWeight, markHeight
+ markWeight = 200
+ markHeight = 72
+
+ var johnWeight, johnHeight
+ johnWeight = 250
+ johnHeight = 76
+
+ var markBmi = markWeight / markHeight^2
+
+ var johnBmi = johnWeight /johnHeight^2
+
+ var markIsHeavier = markBmi > johnBmi
+
+ 
