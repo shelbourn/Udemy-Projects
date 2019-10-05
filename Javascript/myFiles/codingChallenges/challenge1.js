5cfe29c8478b5b2f39fc727b2950a462a1fcd011
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-04 23:20:28 -0700

    Experimenting with alerts and prompts.

--- Javascript/myFiles/codingChallenges/challenge1.js
+++ Javascript/myFiles/codingChallenges/challenge1.js
@@ -2,18 +2,23 @@
  * Coding Challenge 1 - BMI
  */
 
- var markWeight, markHeight
- markWeight = 200
- markHeight = 72
+//  var markWeight, markHeight
+//  markWeight = 200
+//  markHeight = 72
 
- var johnWeight, johnHeight
- johnWeight = 250
- johnHeight = 76
+//  var johnWeight, johnHeight
+//  johnWeight = 250
+//  johnHeight = 76
 
- var markBmi = markWeight / markHeight^2
+//  var markBmi = markWeight / markHeight^2
 
- var johnBmi = johnWeight /johnHeight^2
+//  var johnBmi = johnWeight /johnHeight^2
 
- var markIsFatter = markBmi > johnBmi
+//  var markIsFatter = markBmi > johnBmi
 
- console.log("Is Mark's BMI greater than John's?" + ' ' + markIsFatter)
+//  console.log("Is Mark's BMI greater than John's?" + ' ' + markIsFatter)
+
+var yourWeight = prompt('What is your weight?')
+var yourHeight = prompt('What is your height?')
+
+var yourBmi = alert('Your BMI is ' + yourWeight / yourHeight ^ 2)
