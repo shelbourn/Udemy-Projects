0ffc4289c74392a5519abdead8e264462088167a
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-06 14:34:27 -0700

    If statement with else.

--- Javascript/myFiles/script.js
+++ Javascript/myFiles/script.js
@@ -139,8 +139,17 @@
  *************************************/
 
 var firstName = 'John'
-var civilStatus = 'married'
+var civilStatus = 'single'
 
 if (civilStatus === 'married') {
 	console.log((firstName = ' is married!'))
+} else {
+	console.log(firstName + ' will hopefully marry soon :)')
+}
+
+var isMarried = true
+if (isMarried === true) {
+	console.log((firstName = ' is married!'))
+} else {
+	console.log(firstName + ' will hopefully marry soon :)')
 }
