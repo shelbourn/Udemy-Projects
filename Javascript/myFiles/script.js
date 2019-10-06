8f433c4897a1fb21d18d26f02a6462c459760f64
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-06 15:15:31 -0700

    Updating with multiple else-ifs.

--- Javascript/myFiles/script.js
+++ Javascript/myFiles/script.js
@@ -183,13 +183,15 @@
  *************************************/
 
 var firstName = 'John'
-var age = 10
+var age = 20
 
 if (age < 13) {
 	console.log(firstName + ' is a boy.')
 } else if (age >= 13 && age < 20) {
 	// Between 13 and 20 === age >= 13 AND age < 20
 	console.log(firstName + ' is a teenage boy.')
+} else if (age >= 20 && age < 30) {
+	console.log(firstName + ' is a young man.')
 } else {
 	console.log(firstName + ' is a man.')
 }
