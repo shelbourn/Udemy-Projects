10b8eb41ea1d1926154488db9b32d405c20b1e6e
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-06 15:45:44 -0700

    Ternary operators and switch statemtents.

--- Javascript/myFiles/script.js
+++ Javascript/myFiles/script.js
@@ -182,16 +182,20 @@
  * Boolean Logic
  *************************************/
 
-var firstName = 'John'
-var age = 20
+// var firstName = 'John'
+// var age = 20
 
-if (age < 13) {
-	console.log(firstName + ' is a boy.')
-} else if (age >= 13 && age < 20) {
-	// Between 13 and 20 === age >= 13 AND age < 20
-	console.log(firstName + ' is a teenage boy.')
-} else if (age >= 20 && age < 30) {
-	console.log(firstName + ' is a young man.')
-} else {
-	console.log(firstName + ' is a man.')
-}
+// if (age < 13) {
+// 	console.log(firstName + ' is a boy.')
+// } else if (age >= 13 && age < 20) {
+// 	// Between 13 and 20 === age >= 13 AND age < 20
+// 	console.log(firstName + ' is a teenage boy.')
+// } else if (age >= 20 && age < 30) {
+// 	console.log(firstName + ' is a young man.')
+// } else {
+// 	console.log(firstName + ' is a man.')
+// }
+
+/************************************
+ * The Ternary Operator and Switch Statements
+ *************************************/
