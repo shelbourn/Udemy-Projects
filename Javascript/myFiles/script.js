640306061615c36b58cb44346866e24253596c56
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-06 14:36:04 -0700

    Cleaning code.

--- Javascript/myFiles/script.js
+++ Javascript/myFiles/script.js
@@ -142,14 +142,14 @@ var firstName = 'John'
 var civilStatus = 'single'
 
 if (civilStatus === 'married') {
-	console.log((firstName = ' is married!'))
+	console.log((firstName + ' is married!'))
 } else {
 	console.log(firstName + ' will hopefully marry soon :)')
 }
 
 var isMarried = true
 if (isMarried === true) {
-	console.log((firstName = ' is married!'))
+	console.log((firstName + ' is married!'))
 } else {
 	console.log(firstName + ' will hopefully marry soon :)')
 }
