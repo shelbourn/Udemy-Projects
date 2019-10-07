ce7619667dc4787975e61f9dff5a55dfbc174c82
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-06 22:10:04 -0700

    Truthy and flasy values.

--- Javascript/myFiles/script.js
+++ Javascript/myFiles/script.js
@@ -263,3 +263,16 @@
 /************************************
  * Truthy and Falsy Values and Equality Operators
  *************************************/
+
+// falsy values: undefined, null, 0, '', NaN
+
+// truthy values: NOT falsy values
+
+var height
+
+height = 23
+if (height) {
+	console.log('Variable is defined.')
+} else {
+	console.log('Variable has NOT been defined.')
+}
