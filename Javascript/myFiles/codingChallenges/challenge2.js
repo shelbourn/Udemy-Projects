b54df3cab81552182c1797986999b8def980dbc0
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-07 22:56:31 -0700

    Switch statement to handle this challenge.

--- Javascript/myFiles/codingChallenges/challenge2.js
+++ Javascript/myFiles/codingChallenges/challenge2.js
@@ -1,7 +1,7 @@
 // Challenge 2
 var johnTeamAvg = (89 + 120 + 103) / 3
 var mikeTeamAvg = (89 + 120 + 103) / 3
-var maryTeamAvg = (97 + 134 + 105) / 3
+var maryTeamAvg = (89 + 120 + 103) / 3
 
 switch (true) {
 	case johnTeamAvg > (mikeTeamAvg && maryTeamAvg):
@@ -17,8 +17,14 @@ switch (true) {
 				mikeTeamAvg +
 				' points!'
 		)
+	case maryTeamAvg > (johnTeamAvg && mikeTeamAvg):
+		console.log(
+			"Mary's teams is the winna winna chicken dinna with an average score of " +
+				maryTeamAvg +
+				' points!'
+		)
 		break
-	case (mikeTeamAvg === johnTeamAvg) === maryTeamAvg:
+	default:
 		console.log(
 			"John, Mary, and Mike's team are ready for a rematch...TO THE DEATH with all teams having an average score of " +
 				johnTeamAvg +
