0d1256eb077d8b8cc1398f21e649ad55d0972a6c
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-07 23:51:51 -0700

    Functions.

--- Javascript/myFiles/script.js
+++ Javascript/myFiles/script.js
@@ -292,3 +292,20 @@
 /************************************
  * Functions
  *************************************/
+
+function calculateAge(birthYear) {
+	return 2018 - birthYear
+}
+
+var ageJohn = calculateAge(1990)
+var ageMike = calculateAge(1948)
+var ageJane = calculateAge(1969)
+
+console.log(ageJohn, ageMike, ageJane)
+
+function yearsUntilRetirement(year, firstName) {
+	var age = calculateAge(year)
+	var retirement = 65 - age
+	console.log(firstName + ' retires in ' + retirement + ' years.')
+}
+yearsUntilRetirement(1990, 'John')
