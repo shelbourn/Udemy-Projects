0be4a0d72ca9a3da43aa69484db8e3fc9d9ee9ba
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-07 22:42:55 -0700

    Challenge 2

--- Javascript/myFiles/codingChallenges/challenge2.js
+++ Javascript/myFiles/codingChallenges/challenge2.js
@@ -1,1 +1,16 @@
 // Challenge 2
+var johnTeamAvg = (89 + 120 + 103) / 3
+var mikeTeamAvg = (116 + 94 + 123) / 3
+
+switch (true) {
+	case johnTeamAvg > mikeTeamAvg:
+		console.log("John's team is the winna winna chicken dinna!")
+		break
+	case mikeTeamAvg > johnTeamAvg:
+		console.log("Mike's team is the winna winna chicken dinna!")
+		break
+	case mikeTeamAvg === johnTeamAvg:
+		console.log(
+			"John and Mike's team are ready for a rematch...TO THE DEATH!!!"
+		)
+}
