e019f17710da726fdce40883cafe17298ab19465
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-07 22:51:19 -0700

    Updating code for 3 teams.

--- Javascript/myFiles/codingChallenges/challenge2.js
+++ Javascript/myFiles/codingChallenges/challenge2.js
@@ -1,25 +1,26 @@
 // Challenge 2
 var johnTeamAvg = (89 + 120 + 103) / 3
 var mikeTeamAvg = (89 + 120 + 103) / 3
+var maryTeamAvg = (97 + 134 + 105) / 3
 
 switch (true) {
-	case johnTeamAvg > mikeTeamAvg:
+	case johnTeamAvg > (mikeTeamAvg && maryTeamAvg):
 		console.log(
 			"John's team is the winna winna chicken dinna with an average score of " +
 				johnTeamAvg +
 				' points!'
 		)
 		break
-	case mikeTeamAvg > johnTeamAvg:
+	case mikeTeamAvg > (johnTeamAvg && maryTeamAvg):
 		console.log(
 			"Mike's team is the winna winna chicken dinna with an average score of " +
 				mikeTeamAvg +
 				' points!'
 		)
 		break
-	case mikeTeamAvg === johnTeamAvg:
+	case (mikeTeamAvg === johnTeamAvg) === maryTeamAvg:
 		console.log(
-			"John and Mike's team are ready for a rematch...TO THE DEATH with both teams having an average score of " +
+			"John, Mary, and Mike's team are ready for a rematch...TO THE DEATH with all teams having an average score of " +
 				johnTeamAvg +
 				' points!'
 		)
