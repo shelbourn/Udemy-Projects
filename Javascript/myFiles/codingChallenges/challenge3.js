8ff6ee55554d83584b5655a737286e361e2efce1
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-09 00:15:30 -0700

    Commenting out Challenge 3 code.

--- Javascript/myFiles/codingChallenges/challenge3.js
+++ Javascript/myFiles/codingChallenges/challenge3.js
@@ -22,22 +22,22 @@
 // console.log(tips, totalBills)
 
 // Instructor's Solution
-function calcTip(bill) {
-	var percentage
-	if (bill < 50) percentage = 0.2
-	else if (bill >= 50 && bill < 200) {
-		percentage = 0.15
-	} else {
-		percentage = 0.1
-	}
-	return percentage * bill
-}
+// function calcTip(bill) {
+// 	var percentage
+// 	if (bill < 50) percentage = 0.2
+// 	else if (bill >= 50 && bill < 200) {
+// 		percentage = 0.15
+// 	} else {
+// 		percentage = 0.1
+// 	}
+// 	return percentage * bill
+// }
 
-var bills = [124, 48, 268]
+// var bills = [124, 48, 268]
 
-// Calculating tip based on bills array using function calcTip at specified array index
-var tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
-var totalBill = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
-console.log(bills)
-console.log(tips)
-console.log(totalBill)
+// // Calculating tip based on bills array using function calcTip at specified array index
+// var tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+// var totalBill = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+// console.log(bills)
+// console.log(tips)
+// console.log(totalBill)
