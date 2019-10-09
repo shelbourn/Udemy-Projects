b06f28e2aae47b8a771cb97df125004dec4fc275
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-09 00:09:35 -0700

    Adding instructor's solution

--- Javascript/myFiles/codingChallenges/challenge3.js
+++ Javascript/myFiles/codingChallenges/challenge3.js
@@ -1,24 +1,41 @@
 // My Solution
-function calcTip(bill) {
-	if (bill < 50) {
-		return bill * 0.2
-	} else if (bill >= 50 && bill < 200) {
-		return bill * 0.15
-	} else {
-		return bill * 0.1
-	}
-}
+// function calcTip(bill) {
+// 	if (bill < 50) {
+// 		return bill * 0.2
+// 	} else if (bill >= 50 && bill < 200) {
+// 		return bill * 0.15
+// 	} else {
+// 		return bill * 0.1
+// 	}
+// }
 
-var bill1 = 124
-var bill2 = 48
-var bill3 = 268
+// var bill1 = 124
+// var bill2 = 48
+// var bill3 = 268
 
-var tip1 = calcTip(bill1)
-var tip2 = calcTip(bill2)
-var tip3 = calcTip(bill3)
+// var tip1 = calcTip(bill1)
+// var tip2 = calcTip(bill2)
+// var tip3 = calcTip(bill3)
 
-var tips = [tip1, tip2, tip3]
-var totalBills = [bill1 + tip1, bill2 + tip2, bill3 + tip3]
-console.log(tips, totalBills)
+// var tips = [tip1, tip2, tip3]
+// var totalBills = [bill1 + tip1, bill2 + tip2, bill3 + tip3]
+// console.log(tips, totalBills)
 
 // Instructor's Solution
+function calcTip(bill) {
+	var percentage
+	if (bill < 50) percentage = 0.2
+	else if (bill >= 50 && bill < 200) {
+		percentage = 0.15
+	} else {
+		percentage = 0.1
+	}
+	return percentage * bill
+}
+
+var bills = [124, 48, 268]
+
+// Calculating tip based on bills array using function calcTip at specified array index
+var tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+console.log(bills)
+console.log(tips)
