7c7d9580c149bf917914142917c73b5fa5570f07
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-17 01:57:40 -0700

    I'm stuck

--- Javascript/myFiles/codingChallenges/challenge4.js
+++ Javascript/myFiles/codingChallenges/challenge4.js
@@ -1,17 +1,16 @@
 var john = {
 	mass: 90,
 	height: 2,
-	BMI: function() {
-		johnBMI = this.mass / (this.height ^ 2)
+	johnBmi: function() {
+		this.bmi = this.mass / (this.height ^ 2)
 	},
 }
-john.BMI()
 var mark = {
 	mass: 100,
 	height: 2.5,
-	BMI: function() {
-		markBMI = this.mass / (this.height ^ 2)
+	markBmi: function() {
+		this.bmi = this.mass / (this.height ^ 2)
 	},
 }
-mark.BMI()
-console.log(john.BMI(), mark.BMI())
+console.log(john.johnBmi(), mark.markBmi())
+console.log(john)
