f74a122e10421bf20c4956e6a4a2f151dfaf9800
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-17 01:32:55 -0700

    Updating code.

--- Javascript/myFiles/codingChallenges/challenge4.js
+++ Javascript/myFiles/codingChallenges/challenge4.js
@@ -2,17 +2,17 @@ var john = {
 	mass: 90,
 	height: 2,
 	johnBMI: function() {
-		;(this.mass / this.height) ^ 2
+		this.mass / (this.height ^ 2)
 	},
 }
 var mark = {
 	mass: 100,
 	height: 2.5,
 	markBMI: function() {
-		;(this.mass / this.height) ^ 2
+		this.mass / (this.height ^ 2)
 	},
 }
 
 john.johnBMI()
 mark.markBMI()
-console.log(john.johnBMI, mark.markBMI)
+console.log(john.johnBMI(), mark.markBMI())
