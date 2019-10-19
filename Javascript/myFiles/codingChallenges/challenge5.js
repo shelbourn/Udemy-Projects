1422f4a28af3cb4b6ff4904409c650c28223641a
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-19 00:31:41 -0700

    Challenge 5

--- Javascript/myFiles/codingChallenges/challenge5.js
+++ Javascript/myFiles/codingChallenges/challenge5.js
@@ -2,13 +2,13 @@ var dining = {
 	bills: [124, 48, 268, 180, 42],
 	calcTip: function() {
 		for (i = bills.length - 1; i < this.bills.length; i++) {
-            if(bills[i]<50){
-                this.tip = bills[i]*0.20
-            }else if(bills[i]>=50 ||bills[i]<=200{
-                tip = bills[i]*0.15
-            }else{
-                tip = bills[i]*0.10
-            }
-        }
+			if (bills[i] < 50) {
+				this.tip = bills[i] * 0.2
+			} else if (bills[i] >= 50 || bills[i] <= 200) {
+				this.tip = bills[i] * 0.15
+			} else {
+				this.tip = bills[i] * 0.1
+			}
+		}
 	},
 }
