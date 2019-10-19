098912c23bd792b7207a6a3bf4e5358128555433
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-19 15:56:50 -0700

    Hoisting with variables.

--- Javascript/myFiles/section3Script.js
+++ Javascript/myFiles/section3Script.js
@@ -2,11 +2,31 @@
  * Hoisting in Practice
  */
 
+// Functions
+
 // Function declaration is stored in the 'Global Variable' object even before the code executes.
 // This is why you can call 'calcAge(1965)' before the calcAge() function line is even written
+
+// Function declaration
 
 calcAge(1965)
 
 function calcAge(year) {
 	console.log(2016 - year)
 }
+
+// Function Expression
+
+// retirement(1965) // Does not work with function expressions
+
+var retirement = function(year) {
+	console.log(65 - (2016 - year))
+}
+
+retirement(1990) // This works with function expressions
+
+// Variables
+
+console.log(age)
+var age = 23
+console.log(age)
