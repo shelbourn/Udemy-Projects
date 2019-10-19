17026bc4d0540609495fb9499448fd4c7041f6e9
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-19 15:43:34 -0700

    Hoisting.

--- Javascript/myFiles/section3Script.js
+++ Javascript/myFiles/section3Script.js
@@ -1,3 +1,12 @@
 /************************
  * Hoisting in Practice
  */
+
+// Function declaration is stored in the 'Global Variable' object even before the code executes.
+// This is why you can call 'calcAge(1965)' before the calcAge() function line is even written
+
+calcAge(1965)
+
+function calcAge(year) {
+	console.log(2016 - year)
+}
