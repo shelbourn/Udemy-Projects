58a0ca92007e62139d2eab470b95f3761f1c9659
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-19 00:58:06 -0700

    Drying code.

--- Javascript/myFiles/codingChallenges/challenge5.js
+++ Javascript/myFiles/codingChallenges/challenge5.js
@@ -5,14 +5,15 @@ var dining = {
 		for (i = 0; i < this.bills.length; i++) {
 			if (this.bills[i] < 50) {
 				tip = this.bills[i] * 0.2
-				this.tips.push(tip)
+				// this.tips.push(tip)
 			} else if (this.bills[i] >= 50 || this.bills[i] <= 200) {
 				tip = this.bills[i] * 0.15
-				this.tips.push(tip)
+				// this.tips.push(tip)
 			} else {
 				tip = this.bills[i] * 0.1
-				this.tips.push(tip)
+				// this.tips.push(tip)
 			}
+			this.tips.push(tip)
 		}
 	},
 }
