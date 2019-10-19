576d93e3e9336ef776c6daa13b2e24f8502f5986
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-19 11:42:47 -0700

    Updating code per solution

--- Javascript/myFiles/codingChallenges/challenge5-pt1.js
+++ Javascript/myFiles/codingChallenges/challenge5-pt1.js
@@ -5,16 +5,18 @@ var johnDining = {
 		this.tips = []
 		this.totalBill = []
 		for (var i = 0; i < this.bills.length; i++) {
+			// Determine percentange based on tipping rules
+
+			var percentange
+
 			if (this.bills[i] < 50) {
-				tip = this.bills[i] * 0.2
-				// this.tips.push(tip)
 			} else if (this.bills[i] >= 50 && this.bills[i] <= 200) {
 				tip = this.bills[i] * 0.15
-				// this.tips.push(tip)
 			} else {
 				tip = this.bills[i] * 0.1
-				// this.tips.push(tip)
 			}
+
+			// Add results to the corresponding arrarys
 			this.tips.push(tip)
 			this.totalBill.push(this.bills[i] + this.tips[i])
 		}
