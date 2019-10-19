24137a0bdeecb24bf44025fcc7ff529ea470c655
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-19 11:32:24 -0700

    I give up. I'm checking the answer.

--- Javascript/myFiles/codingChallenges/challenge5-pt2.js
+++ Javascript/myFiles/codingChallenges/challenge5-pt2.js
@@ -1,28 +1,37 @@
-var markBills = [77, 375, 110, 45]
-var markTips = []
-var markDining = function() {
-	for (i = 0; i < markBills.length; i++) {
-		if (markBills[i] < 100) {
-			markBills[i] * 0.2
-		} else if (markBills[i] >= 100 && markbills[i] <= 300) {
-			markBills[i] * 0.1
-		} else {
-			markBills[i] * 0.25
-		}
-	}
-	markTips.push(markDining())
-}
-console.log(markTips)
-console.log(markBills)
+/***************************
+ * My Solution
+ */
 
-var sumTip = 0
+// var markBills = [77, 375, 110, 45]
+// var markTips = []
+// var markDining = function() {
+// 	for (i = 0; i < markBills.length; i++) {
+// 		if (markBills[i] < 100) {
+// 			tip = markBills[i] * 0.2
+// 		} else if (markBills[i] >= 100 && markBills[i] <= 300) {
+// 			tip = markBills[i] * 0.1
+// 		} else {
+// 			tip = markBills[i] * 0.25
+// 		}
+// 	}
+// 	markTips.push(tip)
+// }
 
-var markTip = function() {
-	for (i = 0; i < markBills.length; i++) {
-		sumTip += markBills[i]
-	}
-	return sumTip
-}
-console.log(markTip())
+// console.log(markTips)
+// console.log(markBills)
 
-var avgTip = function() {}
+// var sumTip = 0
+
+// var markTip = function() {
+// 	for (i = 0; i < markTips.length; i++) {
+// 		sumTip += markTips[i]
+// 	}
+// 	return sumTip
+// }
+// console.log(sumTip)
+
+// var avgTip = function() {}
+
+/*****************************
+ * Instructor's Solution
+ */
