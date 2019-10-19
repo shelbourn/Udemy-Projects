50f24e841caed9d4a4943dceb62699ad5c4dba2c
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-19 11:12:09 -0700

    Cleaning code.

--- Javascript/myFiles/codingChallenges/challenge5-pt2.js
+++ Javascript/myFiles/codingChallenges/challenge5-pt2.js
@@ -1,4 +1,21 @@
 var markBills = [77, 375, 110, 45]
+var markTips = []
+var markDining = function() {
+	for (i = 0; i < markBills.length; i++) {
+		if (markBills[i] < 100) {
+			markBills[i] * 0.2
+		} else if (markBills[i] >= 100 && markbills[i] <= 300) {
+			markBills[i] * 0.1
+		} else {
+			markBills[i] * 0.25
+		}
+	}
+	markTips.push(markDining())
+}
+console.log(markTips)
+console.log(markBills)
+
+var markBills = [77, 375, 110, 45]
 var sumTip = 0
 
 var markTip = function() {
