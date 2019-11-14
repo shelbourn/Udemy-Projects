fca2a088f038d5d60a386e845ba7a2bd9b29344f
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-11-13 23:24:33 -0800

    Escape sequences.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/basics-01-starting-project/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/basics-01-starting-project/assets/scripts/app.js
@@ -4,5 +4,6 @@ let currentResult = defaultResult // currentResult is a copy of defaultResult
 currentResult = ((currentResult + 9) * 3) / 2 - 1
 
 let calculateDescription = `(${defaultResult} + 10) * 3 / 2 - 1` // backticks with curly braces for variables within strings (Template Literal)
+let errorMessage = 'An error \n' + 'occurred!'
 
-outputResult(currentResult, calculateDescription)
+outputResult(currentResult, errorMessage)
