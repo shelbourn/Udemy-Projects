f426553ce781e64a7dc3a350689f59b09364c196
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-11-14 12:54:06 -0800

    Using shorthand operators for math operations.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/basics-assignment-1-start/assignment.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/basics-assignment-1-start/assignment.js
@@ -3,11 +3,14 @@ let currentResult
 
 currentResult = 18 + userInput
 
-currentResult = currentResult - 18
+// currentResult = currentResult - 18
+currentResult -= 18
 
-currentResult = currentResult * 12
+// currentResult = currentResult * 12
+currentResult *= 12
 
-currentResult = currentResult / 3
+// currentResult = currentResult / 3
+currentResult /= 3
 
 alert(`The user\'s input is ${userInput}`) // Template String Method
 alert(`The current result is ${currentResult}`) // Template String Method
