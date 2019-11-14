00540e9df2dbc5dcd6ac5598e5da070360252b53
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-11-13 23:28:25 -0800

    Reverting test code.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/basics-01-starting-project/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/basics-01-starting-project/assets/scripts/app.js
@@ -4,6 +4,5 @@ let currentResult = defaultResult // currentResult is a copy of defaultResult
 currentResult = ((currentResult + 9) * 3) / 2 - 1
 
 let calculateDescription = `(${defaultResult} + 10) * 3 / 2 - 1` // backticks with curly braces for variables within strings (Template Literal)
-let errorMessage = 'An error \n' + 'occurred!'
 
-outputResult(currentResult, errorMessage)
+outputResult(currentResult, calculateDescription)
