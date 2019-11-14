c2ad2899582a0929b3f1dbcb09ccaedf07da241c
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-11-14 13:42:06 -0800

    Commenting code.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/basics-06-basic-function/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/basics-06-basic-function/assets/scripts/app.js
@@ -6,6 +6,8 @@ function add(num1, num2) {
 	alert(`The result is ${result}.`)
 }
 
+add() // Calling the function
+
 currentResult = ((currentResult + 10) * 3) / 2 - 1
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
