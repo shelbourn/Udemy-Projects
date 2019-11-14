16408ca53e8168fc0e39216dc135e2277deddbbb
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-11-14 13:38:55 -0800

    Functions

--- Javascript/JSTheCompleteGuideMax/instructorAssets/basics-06-basic-function/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/basics-06-basic-function/assets/scripts/app.js
@@ -1,6 +1,11 @@
 const defaultResult = 0
 let currentResult = defaultResult
 
+function add(num1, num2) {
+	const result = num1 + num2
+	alert(`The result is ${result}.`)
+}
+
 currentResult = ((currentResult + 10) * 3) / 2 - 1
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
