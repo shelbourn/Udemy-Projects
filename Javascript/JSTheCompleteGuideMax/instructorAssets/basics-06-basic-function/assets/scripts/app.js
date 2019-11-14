c5f39bad2cb5717c2e108fac363c5bb53ab3aeb1
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-11-14 14:03:00 -0800

    Demonstrating hoisting.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/basics-06-basic-function/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/basics-06-basic-function/assets/scripts/app.js
@@ -1,13 +1,19 @@
 const defaultResult = 0
 let currentResult = defaultResult
 
-function add(num1, num2) {
-	const result = num1 + num2
-	return result
-}
+// function add(num1, num2) {
+// 	const result = num1 + num2
+// 	return result
+// }
 
 currentResult = add(1, 2) // Calling the function
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
 
 outputResult(currentResult, calculationDescription)
+
+function add(num1, num2) {
+	// Can declare function after it is called due to hoisting
+	const result = num1 + num2
+	return result
+}
