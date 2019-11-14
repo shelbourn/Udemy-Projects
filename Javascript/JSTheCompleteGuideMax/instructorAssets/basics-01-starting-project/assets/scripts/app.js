d5ee8451b5b99af18fcdfd9949369cf3a405d2f6
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-11-13 23:13:20 -0800

    Template literal

--- Javascript/JSTheCompleteGuideMax/instructorAssets/basics-01-starting-project/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/basics-01-starting-project/assets/scripts/app.js
@@ -3,6 +3,6 @@ let currentResult = defaultResult // currentResult is a copy of defaultResult
 
 currentResult = ((currentResult + 9) * 3) / 2 - 1
 
-let calculateDescription = `(${defaultResult} + 10) * 3 / 2 - 1` // backticks with curly braces for variables within strings
+let calculateDescription = `(${defaultResult} + 10) * 3 / 2 - 1` // backticks with curly braces for variables within strings (Template Literal)
 
 outputResult(currentResult, calculateDescription)
