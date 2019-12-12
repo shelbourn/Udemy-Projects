33ef5e8f7500429d96e7bee900ec2c80f9c5b2a9
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-11 16:16:46 -0800

    MOD (Modulus) functions.

--- oracleDB12cSQL/databases/hr.sql
+++ oracleDB12cSQL/databases/hr.sql
@@ -654,3 +654,6 @@ select trunc (10.6565, 2) from dual; -- Truncates the two decimals after the fir
 select trunc (998.6565, -2) from dual; -- Truncates (turns to zeroes) the digits two places to the left of the decimal place
 
 select trunc (998.6565, -3) from dual;
+
+-- MOD Functions
+-- Modulus: Returns the remainder of dividing two numbers
