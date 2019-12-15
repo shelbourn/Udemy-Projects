0de062a760bc05fb8fdecdca01a6e90b507cab7b
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-15 14:18:19 -0800

    Assignment 2 completed.

--- Javascript/JSTheCompleteGuideMax/basics-assignment-2-start/basics-assignment-2-start/assignment.js
+++ Javascript/JSTheCompleteGuideMax/basics-assignment-2-start/basics-assignment-2-start/assignment.js
@@ -8,8 +8,12 @@ function nameAlert2(name) {
 	alert(name)
 }
 
+function nameAlert3(intro, body, end) {
+	return alert(intro + ' ' + body + ' ' + end)
+}
+
 // Cannot return a function when it is being used for an event listener
 // nameAlert1()
 nameAlert2('Matt')
-
 task3Element.addEventListener('click', nameAlert1)
+nameAlert3('Hello!', 'My name is Matt, nice to meet you.', 'Have a great day!')
