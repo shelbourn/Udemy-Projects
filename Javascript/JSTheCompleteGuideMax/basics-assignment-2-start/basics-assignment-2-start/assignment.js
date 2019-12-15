aea519219c6452cd5d8980253927687bbacce6c0
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-15 14:05:38 -0800

    Assignment number 2.

--- Javascript/JSTheCompleteGuideMax/basics-assignment-2-start/basics-assignment-2-start/assignment.js
+++ Javascript/JSTheCompleteGuideMax/basics-assignment-2-start/basics-assignment-2-start/assignment.js
@@ -1,1 +1,9 @@
-const task3Element = document.getElementById('task-3');
+const task3Element = document.getElementById('task-3')
+
+function nameAlert1() {
+	alert('Hi! My name is Matt, nice to meet you.')
+}
+
+function nameAlert2(name) {
+	alert(name)
+}
