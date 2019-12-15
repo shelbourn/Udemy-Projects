d455d825bfe1935197e77553e7af3bfac562783a
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-14 22:39:47 -0800

    Shadow Variables.

--- Javascript/JSTheCompleteGuideMax/myCode.js
+++ Javascript/JSTheCompleteGuideMax/myCode.js
@@ -18,3 +18,9 @@ currentResult = add(1, 2)
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
 
 outputResult(currentResult, calculationDescription)
+
+/* SHADOW VARIABLES
+
+When referring to userName inside of the greetUser function we now
+always refer to the local, shadowed variable. Only if no such local variable existed, JavaScript would fall back to the global variable.
+*/
