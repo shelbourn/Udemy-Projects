591de88a91233497663de29eb8cbf3b87c879863
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-15 14:45:50 -0800

    Assignment 2 edits.

--- Javascript/JSTheCompleteGuideMax/basics-assignment-2-start/basics-assignment-2-start/assignment.js
+++ Javascript/JSTheCompleteGuideMax/basics-assignment-2-start/basics-assignment-2-start/assignment.js
@@ -9,11 +9,28 @@ function nameAlert2(name) {
 }
 
 function nameAlert3(intro, body, end) {
-	return alert(intro + ' ' + body + ' ' + end)
+	// return intro + ' ' + body + ' ' + end
+	return `${intro} ${body} ${end}`
 }
 
 // Cannot return a function when it is being used for an event listener
 // nameAlert1()
+
 nameAlert2('Matt')
-task3Element.addEventListener('click', nameAlert1)
-nameAlert3('Hello!', 'My name is Matt, nice to meet you.', 'Have a great day!')
+// task3Element.addEventListener('click', nameAlert1)
+// alert(
+// 	nameAlert3(
+// 		'Hello!',
+// 		'My name is Matt, nice to meet you.',
+// 		'Have a great day!'
+// 	)
+// )
+
+// Below is a best practice and more readable
+const concatAlert = nameAlert3(
+	'Hello!',
+	'My name is Matt, nice to meet you.',
+	'Have a great day!'
+)
+
+alert(concatAlert)
