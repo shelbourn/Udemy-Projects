0f8b2e5ed150e77c92133f40160f114c7e078533
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-15 16:48:04 -0800

    Converting data types.

--- Javascript/JSTheCompleteGuideMax/myCode.js
+++ Javascript/JSTheCompleteGuideMax/myCode.js
@@ -36,32 +36,36 @@
 // execution.
 // */
 
-// Executing Functions "Indirectly"
+// // Executing Functions "Indirectly"
 
-const defaultResult = 0
-let currentResult = defaultResult
+// const defaultResult = 0
+// let currentResult = defaultResult
 
-// Function has no return statement because it is just feeding
-// information to the event listener
-// It also alters a global variable, but since it doesn't return it
-// it indicates to the programmer that it is not to be used for anything
-// besides the event listener
-// Function remains active after script has run so the
-// outputResult variable must be included in the function for the code to work
-// Functions without return tell the JS engine to not execute the function immediately
-// but to store it in memory for future execution upon an event
-// JS ALWAYS EXECUTES A FUNCTION WHEN IT INCLUDES PARENS
-// This is why with event listeners only the function's name is used and not parens
+// // Function has no return statement because it is just feeding
+// // information to the event listener
+// // It also alters a global variable, but since it doesn't return it
+// // it indicates to the programmer that it is not to be used for anything
+// // besides the event listener
+// // Function remains active after script has run so the
+// // outputResult variable must be included in the function for the code to work
+// // Functions without return tell the JS engine to not execute the function immediately
+// // but to store it in memory for future execution upon an event
+// // JS ALWAYS EXECUTES A FUNCTION WHEN IT INCLUDES PARENS
+// // This is why with event listeners only the function's name is used and not parens
 
-function add() {
-	currentResult = currentResult + userInput.value
-	outputResult(currentResult, '')
-}
+// function add() {
+// 	currentResult = currentResult + userInput.value
+// 	outputResult(currentResult, '')
+// }
 
-// Event Listeners take two parameters
-// 1.) String value
-// 2.) Function name WITHOUT PARENS
-addBtn.addEventListener('click', add)
+// // Event Listeners take two parameters
+// // 1.) String value
+// // 2.) Function name WITHOUT PARENS
+// addBtn.addEventListener('click', add)
 
-// Functions with empty parens indicate that the function does not require any parameters.
-// Function parameters are essentially variables to be used within the function
+// // Functions with empty parens indicate that the function does not require any parameters.
+// // Function parameters are essentially variables to be used within the function
+
+// Converting Data Types
+
+// Anything input by a user and imported via an html file will always be recognized by JS as a string
