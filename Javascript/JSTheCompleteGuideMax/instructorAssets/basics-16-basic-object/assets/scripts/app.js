846cad5cd44a261c906ab61b435519594c859ca4
shelbourn <mattshelbourn@gmail.com>
2019-12-20 12:38:42 -0800

    Setting up code for lesson.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/basics-16-basic-object/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/basics-16-basic-object/assets/scripts/app.js
@@ -1,55 +1,84 @@
-const defaultResult = 0;
-let currentResult = defaultResult;
-let logEntries = [];
+const defaultResult = 0
+let currentResult = 0
+let logEntries = []
+
+// Arrays have an index that is zero-based (index starts at zero)
+// Object use key-value pairs. Keys are the identifiers for the elements and those are paired with information/data
 
 // Gets input from input field
 function getUserNumberInput() {
-  return parseInt(usrInput.value);
+	return parseInt(usrInput.value) // Converts String (User Input) to number
 }
 
 // Generates and writes calculation log
-function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
-  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-  outputResult(currentResult, calcDescription); // from vendor file
+// Outputs only text
+function createAndWriteOutput(operator, resultBeforeCal, calcNumber) {
+	const calcDescription = `${resultBeforeCal} ${operator} ${calcNumber}`
+	outputResult(currentResult, calcDescription) // From vendor.js file
 }
 
+// currentResult++ (adds 1 to number) Returns number before 1 is added
+// currentResult-- (subtracts 1 from a number) Returns number before 1 is subtracted
+// ++currentResult (adds 1 to a number) Returns number after 1 is added
+// --currentResult (subtracts 1 from a number) Returns number after 1 is subtracted
+
 function add() {
-  const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
-  currentResult += enteredNumber;
-  createAndWriteOutput('+', initialResult, enteredNumber);
-  const logEntry = {
-    operation: 'ADD',
-    prevResult: initialResult,
-    number: enteredNumber,
-    result: currentResult
-  };
-  logEntries.push(logEntry);
-  console.log(logEntries);
+	const enteredNumber = getUserNumberInput()
+	// const calcDescription = `${currentResult} + ${enteredNumber}`
+	// initialResult will not change if currentResult changes afterwards
+	const initialResult = currentResult
+	currentResult += enteredNumber
+	// currentResult = currentResult + enteredNumber
+	createAndWriteOutput('+', initialResult, enteredNumber)
+	// outputResult(currentResult, calcDescription)
+	// Stores operation and result in an object
+	const logEntry = {
+		oepration: 'ADD',
+		prevResult: initialResult,
+		number: enteredNumber,
+		result: currentResult,
+	}
+	logEntries.push(logEntry) // Pushes logEntry object to logEntries array
+	console.log(logEntries) // Reads array element at index 0
 }
 
 function subtract() {
-  const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
-  currentResult -= enteredNumber;
-  createAndWriteOutput('-', initialResult, enteredNumber);
+	const enteredNumber = getUserNumberInput()
+	// const calcDescription = `${currentResult} - ${enteredNumber}`
+	const initialResult = currentResult
+	currentResult -= enteredNumber
+	// currentResult = currentResult - enteredNumber
+	createAndWriteOutput('-', initialResult, enteredNumber)
+	// outputResult(currentResult, calcDescription)
+	logEntries.push(enteredNumber)
+	console.log(logEntries)
 }
 
 function multiply() {
-  const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
-  currentResult *= enteredNumber;
-  createAndWriteOutput('*', initialResult, enteredNumber);
+	const enteredNumber = getUserNumberInput()
+	// const calcDescription = `${currentResult} * ${enteredNumber}`
+	const initialResult = currentResult
+	currentResult *= enteredNumber
+	// currentResult = currentResult * enteredNumber
+	createAndWriteOutput('*', initialResult, enteredNumber)
+	// outputResult(currentResult, calcDescription)
+	logEntries.push(enteredNumber)
+	console.log(logEntries)
 }
 
 function divide() {
-  const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
-  currentResult /= enteredNumber;
-  createAndWriteOutput('/', initialResult, enteredNumber);
+	const enteredNumber = getUserNumberInput()
+	// const calcDescription = `${currentResult} / ${enteredNumber}`
+	const initialResult = currentResult
+	currentResult /= enteredNumber
+	// currentResult = currentResult / enteredNumber
+	createAndWriteOutput('/', initialResult, enteredNumber)
+	// outputResult(currentResult, calcDescription)
+	logEntries.push(enteredNumber)
+	console.log(logEntries)
 }
 
-addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', subtract);
-multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click', divide);
+addBtn.addEventListener('click', add)
+subtractBtn.addEventListener('click', subtract)
+multiplyBtn.addEventListener('click', multiply)
+divideBtn.addEventListener('click', divide)
