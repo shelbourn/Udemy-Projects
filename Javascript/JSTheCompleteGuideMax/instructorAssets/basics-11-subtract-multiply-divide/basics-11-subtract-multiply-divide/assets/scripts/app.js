f0c441fadadc2ce0216070e63f93fc6b1e299b65
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-19 21:46:00 -0800

    Refactoring code.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/basics-11-subtract-multiply-divide/basics-11-subtract-multiply-divide/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/basics-11-subtract-multiply-divide/basics-11-subtract-multiply-divide/assets/scripts/app.js
@@ -5,32 +5,46 @@ function getUserNumberInput() {
 	return parseInt(usrInput.value) // Converts String (User Input) to number
 }
 
+// Outputs only text
+function createAndWriteOutput(operator, resultBeforeCal, calcNumber) {
+	const calcDescription = `${resultBeforeCal} ${operator} ${calcNumber}`
+	outputResult(currentResult, calcDescription)
+}
+
 function add() {
 	const enteredNumber = getUserNumberInput()
-	const calcDescription = `${currentResult} + ${enteredNumber}`
+	// const calcDescription = `${currentResult} + ${enteredNumber}`
+	const initialResult = currentResult
 	currentResult = currentResult + enteredNumber
-	outputResult(currentResult, calcDescription)
+	createAndWriteOutput('+', initialResult, enteredNumber)
+	// outputResult(currentResult, calcDescription)
 }
 
 function subtract() {
 	const enteredNumber = getUserNumberInput()
-	const calcDescription = `${currentResult} - ${enteredNumber}`
+	// const calcDescription = `${currentResult} - ${enteredNumber}`
+	const initialResult = currentResult
 	currentResult = currentResult - enteredNumber
-	outputResult(currentResult, calcDescription)
+	createAndWriteOutput('-', initialResult, enteredNumber)
+	// outputResult(currentResult, calcDescription)
 }
 
 function multiply() {
 	const enteredNumber = getUserNumberInput()
-	const calcDescription = `${currentResult} * ${enteredNumber}`
+	// const calcDescription = `${currentResult} * ${enteredNumber}`
+	const initialResult = currentResult
 	currentResult = currentResult * enteredNumber
-	outputResult(currentResult, calcDescription)
+	createAndWriteOutput('*', initialResult, enteredNumber)
+	// outputResult(currentResult, calcDescription)
 }
 
 function divide() {
 	const enteredNumber = getUserNumberInput()
-	const calcDescription = `${currentResult} / ${enteredNumber}`
+	// const calcDescription = `${currentResult} / ${enteredNumber}`
+	const initialResult = currentResult
 	currentResult = currentResult / enteredNumber
-	outputResult(currentResult, calcDescription)
+	createAndWriteOutput('/', initialResult, enteredNumber)
+	// outputResult(currentResult, calcDescription)
 }
 
 addBtn.addEventListener('click', add)
