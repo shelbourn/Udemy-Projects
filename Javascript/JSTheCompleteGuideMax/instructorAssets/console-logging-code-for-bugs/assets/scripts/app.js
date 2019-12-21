84c91938d5ea84094895f29d38e5a65177c6153c
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-21 12:19:37 -0800

    Debugging with VS Code.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/console-logging-code-for-bugs/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/console-logging-code-for-bugs/assets/scripts/app.js
@@ -2,28 +2,14 @@ const defaultResult = 0
 let currentResult = 0
 let logEntries = []
 
-/**********************************
- * Debugging with Chrome Dev Tools
+/******************************************
+ * Debugging with VS Code Debugging Tools
  *
  */
 
-// Breakpoints allow you to pause code execution at that point
-// Add breakpoints through Chrome Dev tools in the 'Sources' tab and then click on code file
-// Code execution stops before the line where the breakpoint is added
-// Call stack shows the functions that have been called in order
-// Top function in call stack is function that is running
-// Functions below top function are the functions that are responsible for call the top function
-// Variables can be changed in the 'Scope' window after clicking on a function in the Call Stack
-// You can add variables or expressions to the 'Watch' list in the 'Scope' window to see how they change
-// Conditional breakpoints may be added by right clicking the line in Chrome's debugging window
-// Conditional breakpoints can activate only when certain conditions are met
-// Event listener breakpoints will pause code execution whenever a user interraction occurs based on the criteria you set
-// Changing the code in Chrome's debugger does not alter file on filesystem, only in browser
-// This helps to test fixes before you change your actual files
-
 // Gets input from input field
 function getUserNumberInput() {
-	return parseInt(usrInput.value) // Converts String (User Input) to number. .value is the contents from a form input field
+	return usrInput.value // Converts String (User Input) to number. .value is the contents from a form input field
 }
 
 // Generates and writes calculation log
