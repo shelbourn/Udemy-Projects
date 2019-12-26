03d4385461bb78afeb52a8611c674c5875a892b0
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-25 18:14:25 -0800

    Cleanin up code with if statement.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/control-01-starting-project/control-01-starting-project/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/control-01-starting-project/control-01-starting-project/assets/scripts/app.js
@@ -32,29 +32,24 @@ function writeToLog(
 function calculateResult(calculationType) {
 	const enteredNumber = getUserNumberInput()
 	const initialResult = currentResult
+	let mathOperator
 	if (calculationType === 'ADD') {
 		currentResult += enteredNumber
+		mathOperator = '+'
 	} else {
 		currentResult -= enteredNumber
+		mathOperator = '-'
 	}
-	createAndWriteOutput('+', initialResult, enteredNumber)
+	createAndWriteOutput(mathOperator, initialResult, enteredNumber)
 	writeToLog(calculationType, initialResult, enteredNumber, currentResult)
 }
 
 function add() {
-	const enteredNumber = getUserNumberInput()
-	const initialResult = currentResult
-	currentResult += enteredNumber
-	createAndWriteOutput('+', initialResult, enteredNumber)
-	writeToLog('ADD', initialResult, enteredNumber, currentResult)
+	calculateResult('ADD')
 }
 
 function subtract() {
-	const enteredNumber = getUserNumberInput()
-	const initialResult = currentResult
-	currentResult -= enteredNumber
-	createAndWriteOutput('-', initialResult, enteredNumber)
-	writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult)
+	calculateResult('SUBTRACT')
 }
 
 function multiply() {
