125bd97a664738de73d16940a80c359ee19c4580
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-25 21:32:44 -0800

    Final notes on Falsey and Truthy values.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/control-01-starting-project/control-01-starting-project/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/control-01-starting-project/control-01-starting-project/assets/scripts/app.js
@@ -58,10 +58,11 @@ function writeToLog(
 
 function calculateResult(calculationType) {
 	if (
-		calculationType !== 'ADD' &&
-		calculationType !== 'SUBTRACT' &&
-		calculationType !== 'MULTIPLY' &&
-		calculationType !== 'DIVIDE'
+		(calculationType !== 'ADD' &&
+			calculationType !== 'SUBTRACT' &&
+			calculationType !== 'MULTIPLY' &&
+			calculationType !== 'DIVIDE') ||
+		!enteredNumber
 	) {
 		return
 	}
