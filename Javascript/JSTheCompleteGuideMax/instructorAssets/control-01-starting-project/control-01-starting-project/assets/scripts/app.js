46df52fb231818e568844907f63700226f21de7b
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-25 18:19:21 -0800

    Adding else if statements

--- Javascript/JSTheCompleteGuideMax/instructorAssets/control-01-starting-project/control-01-starting-project/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/control-01-starting-project/control-01-starting-project/assets/scripts/app.js
@@ -36,9 +36,15 @@ function calculateResult(calculationType) {
 	if (calculationType === 'ADD') {
 		currentResult += enteredNumber
 		mathOperator = '+'
-	} else {
+	} else if (calculationType === 'SUBTRACT') {
 		currentResult -= enteredNumber
 		mathOperator = '-'
+	} else if (calculationType === 'MULTIPLY') {
+		currentResult *= enteredNumber
+		mathOperator = '*'
+	} else {
+		currentResult /= enteredNumber
+		mathOperator = '/'
 	}
 	createAndWriteOutput(mathOperator, initialResult, enteredNumber)
 	writeToLog(calculationType, initialResult, enteredNumber, currentResult)
@@ -53,19 +59,11 @@ function subtract() {
 }
 
 function multiply() {
-	const enteredNumber = getUserNumberInput()
-	const initialResult = currentResult
-	currentResult *= enteredNumber
-	createAndWriteOutput('*', initialResult, enteredNumber)
-	writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult)
+	calculateResult('MULTIPLY')
 }
 
 function divide() {
-	const enteredNumber = getUserNumberInput()
-	const initialResult = currentResult
-	currentResult /= enteredNumber
-	createAndWriteOutput('/', initialResult, enteredNumber)
-	writeToLog('DIVIDE', initialResult, enteredNumber, currentResult)
+	calculateResult('DIVIDE')
 }
 
 addBtn.addEventListener('click', add)
