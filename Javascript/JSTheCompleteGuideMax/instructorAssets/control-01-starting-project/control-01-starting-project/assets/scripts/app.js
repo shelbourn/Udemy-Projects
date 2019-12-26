4bf65ce15998ac72a22311f288e99077eabcdf62
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-25 20:23:41 -0800

    Cleaning code and using && operator.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/control-01-starting-project/control-01-starting-project/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/control-01-starting-project/control-01-starting-project/assets/scripts/app.js
@@ -38,6 +38,15 @@ function writeToLog(
 }
 
 function calculateResult(calculationType) {
+	if (
+		calculationType !== 'ADD' &&
+		calculationType !== 'SUBTRACT' &&
+		calculationType !== 'MULTIPLY' &&
+		calculationType !== 'DIVIDE'
+	) {
+		return
+	}
+
 	const enteredNumber = getUserNumberInput()
 	const initialResult = currentResult
 	let mathOperator
