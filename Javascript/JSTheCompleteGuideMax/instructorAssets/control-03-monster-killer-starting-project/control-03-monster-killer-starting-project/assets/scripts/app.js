cde8311c7937e549ab62e49eeac5cc6ca5ebc07d
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-25 22:49:59 -0800

    Adding STRONG_ATTACK_VALUE

--- Javascript/JSTheCompleteGuideMax/instructorAssets/control-03-monster-killer-starting-project/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/control-03-monster-killer-starting-project/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10 // Global value. Uppercase characters separated by underscore for hard-coded values related to gameplay
+const STRONG_ATTACK_VALUE = 17
 const MONSTER_ATTACK_VALUE = 14
 
 let chosenMaxLife = 100 // For player and monster
@@ -22,4 +23,19 @@ function attackHandler() {
 	}
 }
 
+function strongAttackHandler() {
+	const damage = dealMonsterDamage(STRONG_ATTACK_VALUE)
+	currentMonsterHealth -= damage
+	const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
+	currentPlayerHealth -= playerDamage
+	if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+		alert('You won!')
+	} else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+		alert('You lost!')
+	} else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+		alert('You have a draw!')
+	}
+}
+
 attackBtn.addEventListener('click', attackHandler)
+strongAttackBtn.addEventListener('click', strongAttackHandler)
