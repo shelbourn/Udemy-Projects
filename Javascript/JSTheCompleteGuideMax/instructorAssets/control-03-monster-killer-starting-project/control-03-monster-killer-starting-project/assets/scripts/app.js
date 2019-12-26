bc2ec6be3b3770f90b2e3462f2bf6991cb444a0f
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-25 22:20:46 -0800

    Finishing the code for the attack button.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/control-03-monster-killer-starting-project/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/control-03-monster-killer-starting-project/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -13,8 +13,12 @@ function attackHandler() {
 	currentMonsterHealth -= damage
 	const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
 	currentPlayerHealth -= playerDamage
-	if (currentMonsterHealth <= 0) {
+	if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
 		alert('You won!')
+	} else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+		alert('You lost!')
+	} else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+		alert('You have a draw!')
 	}
 }
 
