a8e3a14ccbd321c3631f3eb0ef59febfd8c328ad
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-25 22:12:15 -0800

    Adding code to let monster attack back.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/control-03-monster-killer-starting-project/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/control-03-monster-killer-starting-project/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10 // Global value. Uppercase characters separated by underscore for hard-coded values related to gameplay
+const MONSTER_ATTACK_VALUE = 14
 
 let chosenMaxLife = 100 // For player and monster
 let currentMonsterHealth = chosenMaxLife
@@ -8,8 +9,13 @@ adjustHealthBars(chosenMaxLife)
 
 // Naming convention of function refers to what the function does and what it is connected to
 function attackHandler() {
-	const damage = dealMonsterDamange(ATTACK_VALUE)
+	const damage = dealMonsterDamage(ATTACK_VALUE)
 	currentMonsterHealth -= damage
+	const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
+	currentPlayerHealth -= playerDamage
+	if (currentMonsterHealth <= 0) {
+		alert('You won!')
+	}
 }
 
 attackBtn.addEventListener('click', attackHandler)
