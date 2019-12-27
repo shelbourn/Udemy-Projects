abc57be4fbc7cf1fabbe65ad8ed9b5a026c8eaa5
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-26 19:11:40 -0800

    Adding endRound() function and comments.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/control-03-monster-killer-starting-project/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/control-03-monster-killer-starting-project/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATTACK_VALUE = 10 // Global value. Uppercase characters separated by underscore for hard-coded values related to gameplay
 const STRONG_ATTACK_VALUE = 17
 const MONSTER_ATTACK_VALUE = 14
+const HEAL_VALUE = 20
 
 let chosenMaxLife = 100 // For player and monster
 let currentMonsterHealth = chosenMaxLife
@@ -8,16 +9,9 @@ let currentPlayerHealth = chosenMaxLife
 
 adjustHealthBars(chosenMaxLife)
 
-function attackMonster(mode) {
-	let maxDamage
-	if (mode === 'ATTACK') {
-		maxDamage = ATTACK_VALUE
-	} else if (mode === 'STRONG_ATTACK') {
-		maxDamage = STRONG_ATTACK_VALUE
-	}
-
-	const damage = dealMonsterDamage(maxDamage)
-	currentMonsterHealth -= damage
+// endRound function checks win condition after each round
+// Monster attack
+function endRound() {
 	const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
 	currentPlayerHealth -= playerDamage
 	if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
@@ -29,6 +23,20 @@ function attackMonster(mode) {
 	}
 }
 
+//Player attack
+function attackMonster(mode) {
+	let maxDamage
+	if (mode === 'ATTACK') {
+		maxDamage = ATTACK_VALUE
+	} else if (mode === 'STRONG_ATTACK') {
+		maxDamage = STRONG_ATTACK_VALUE
+	}
+
+	const damage = dealMonsterDamage(maxDamage)
+	currentMonsterHealth -= damage
+	endRound()
+}
+
 // Naming convention of function refers to what the function does and what it is connected to
 function attackHandler() {
 	attackMonster('ATTACK')
@@ -38,8 +46,12 @@ function strongAttackHandler() {
 	attackMonster('STRONG_ATTACK')
 }
 
-function healHandler() {}
+// Heal function
+function healPlayerHandler() {
+	increasePlayerHealth(HEAL_VALUE)
+	endRound()
+}
 
 attackBtn.addEventListener('click', attackHandler)
 strongAttackBtn.addEventListener('click', strongAttackHandler)
-healBtn.addEventListener('click', healHandler)
+healBtn.addEventListener('click', healPlayerHandler)
