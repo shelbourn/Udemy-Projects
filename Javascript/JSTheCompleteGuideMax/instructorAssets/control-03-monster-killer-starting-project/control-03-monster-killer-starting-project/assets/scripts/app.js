ff4e5fc3f448b882748af28556438ebca5f97c25
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-27 21:10:09 -0800

    Switch-Case statement in practice.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/control-03-monster-killer-starting-project/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/control-03-monster-killer-starting-project/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -29,57 +29,97 @@ let hasBonusLife = true
 adjustHealthBars(chosenMaxLife)
 
 function writeToLog(ev, val, monsterHealth, playerHealth) {
-	// One Method for pushing log entries
+	// Switch -Case Statement
+	// Looks for equality
+	// Switch always uses === to compare values
+	// Breaks keyword ends Switch-Case statement from progressing after condition is met
+	// If break keyword is not included and a condition is met then the code in the second case will execute even if conditions aren't met.
 	let logEntry = {
 		event: ev,
 		value: val,
 		finalMonsterHealth: monsterHealth,
 		finalPlayerHealth: playerHealth,
 	}
-
-	// Switch -Case Statement
-	// Looks for equality
-	// Switch always uses === to compare values
 	switch (ev) {
 		case LOG_EVENT_PLAYER_ATTACK:
+			logEntry.target = 'MONSTER'
+			break
+		case LOG_EVENT_PLAYER_STRONG_ATTACK:
+			logEntry = {
+				event: ev,
+				value: val,
+				target: 'MONSTER',
+				finalMonsterHealth: monsterHealth,
+				finalPlayerHealth: playerHealth,
+			}
+			break
+		case LOG_EVENT_MONSTER_ATTACK:
+			logEntry = {
+				event: ev,
+				value: val,
+				target: 'PLAYER',
+				finalMonsterHealth: monsterHealth,
+				finalPlayerHealth: playerHealth,
+			}
+			break
+		case LOG_EVENT_PLAYER_HEAL:
+			logEntry = {
+				event: ev,
+				value: val,
+				target: 'PLAYER',
+				finalMonsterHealth: monsterHealth,
+				finalPlayerHealth: playerHealth,
+			}
+			break
+		case LOG_EVENT_GAME_OVER:
+			logEntry = {
+				event: ev,
+				value: val,
+				finalMonsterHealth: monsterHealth,
+				finalPlayerHealth: playerHealth,
+			}
+			break
+		// Default value if no conditions are met
+		default:
+			logEntry = {}
 	}
 
-	if (ev === LOG_EVENT_PLAYER_ATTACK) {
-		logEntry.target = 'MONSTER'
+	// if (ev === LOG_EVENT_PLAYER_ATTACK) {
+	// 	logEntry.target = 'MONSTER'
 
-		// Second method for pushing log entries
-	} else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-		logEntry = {
-			event: ev,
-			value: val,
-			target: 'MONSTER',
-			finalMonsterHealth: monsterHealth,
-			finalPlayerHealth: playerHealth,
-		}
-	} else if (ev === LOG_EVENT_MONSTER_ATTACK) {
-		logEntry = {
-			event: ev,
-			value: val,
-			target: 'PLAYER',
-			finalMonsterHealth: monsterHealth,
-			finalPlayerHealth: playerHealth,
-		}
-	} else if (ev === LOG_EVENT_PLAYER_HEAL) {
-		logEntry = {
-			event: ev,
-			value: val,
-			target: 'PLAYER',
-			finalMonsterHealth: monsterHealth,
-			finalPlayerHealth: playerHealth,
-		}
-	} else if (ev === LOG_EVENT_GAME_OVER) {
-		logEntry = {
-			event: ev,
-			value: val,
-			finalMonsterHealth: monsterHealth,
-			finalPlayerHealth: playerHealth,
-		}
-	}
+	// 	// Second method for pushing log entries
+	// } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+	// 	logEntry = {
+	// 		event: ev,
+	// 		value: val,
+	// 		target: 'MONSTER',
+	// 		finalMonsterHealth: monsterHealth,
+	// 		finalPlayerHealth: playerHealth,
+	// 	}
+	// } else if (ev === LOG_EVENT_MONSTER_ATTACK) {
+	// 	logEntry = {
+	// 		event: ev,
+	// 		value: val,
+	// 		target: 'PLAYER',
+	// 		finalMonsterHealth: monsterHealth,
+	// 		finalPlayerHealth: playerHealth,
+	// 	}
+	// } else if (ev === LOG_EVENT_PLAYER_HEAL) {
+	// 	logEntry = {
+	// 		event: ev,
+	// 		value: val,
+	// 		target: 'PLAYER',
+	// 		finalMonsterHealth: monsterHealth,
+	// 		finalPlayerHealth: playerHealth,
+	// 	}
+	// } else if (ev === LOG_EVENT_GAME_OVER) {
+	// 	logEntry = {
+	// 		event: ev,
+	// 		value: val,
+	// 		finalMonsterHealth: monsterHealth,
+	// 		finalPlayerHealth: playerHealth,
+	// 	}
+	// }
 	battleLog.push(logEntry)
 }
 
