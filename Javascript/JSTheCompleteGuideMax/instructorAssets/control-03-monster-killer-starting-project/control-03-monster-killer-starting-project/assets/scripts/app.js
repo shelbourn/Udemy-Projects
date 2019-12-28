2db18b515fc219e3a9efa73cd7fba6faf53c5843
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-27 21:24:09 -0800

    Prepping code for next lesson

--- Javascript/JSTheCompleteGuideMax/instructorAssets/control-03-monster-killer-starting-project/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/control-03-monster-killer-starting-project/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -82,43 +82,6 @@ function writeToLog(ev, val, monsterHealth, playerHealth) {
 		default:
 			logEntry = {}
 	}
-
-	// if (ev === LOG_EVENT_PLAYER_ATTACK) {
-	// 	logEntry.target = 'MONSTER'
-
-	// 	// Second method for pushing log entries
-	// } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-	// 	logEntry = {
-	// 		event: ev,
-	// 		value: val,
-	// 		target: 'MONSTER',
-	// 		finalMonsterHealth: monsterHealth,
-	// 		finalPlayerHealth: playerHealth,
-	// 	}
-	// } else if (ev === LOG_EVENT_MONSTER_ATTACK) {
-	// 	logEntry = {
-	// 		event: ev,
-	// 		value: val,
-	// 		target: 'PLAYER',
-	// 		finalMonsterHealth: monsterHealth,
-	// 		finalPlayerHealth: playerHealth,
-	// 	}
-	// } else if (ev === LOG_EVENT_PLAYER_HEAL) {
-	// 	logEntry = {
-	// 		event: ev,
-	// 		value: val,
-	// 		target: 'PLAYER',
-	// 		finalMonsterHealth: monsterHealth,
-	// 		finalPlayerHealth: playerHealth,
-	// 	}
-	// } else if (ev === LOG_EVENT_GAME_OVER) {
-	// 	logEntry = {
-	// 		event: ev,
-	// 		value: val,
-	// 		finalMonsterHealth: monsterHealth,
-	// 		finalPlayerHealth: playerHealth,
-	// 	}
-	// }
 	battleLog.push(logEntry)
 }
 
