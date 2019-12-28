bbdbe8101fa856545772d592da82a72b87b51225
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-12-27 21:43:28 -0800

    For loop examples

--- Javascript/JSTheCompleteGuideMax/instructorAssets/control-03-monster-killer-starting-project/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/control-03-monster-killer-starting-project/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -203,6 +203,14 @@ function printLogHandler() {
 	for (let i = 0; i < 3; i++) {
 		console.log('-------------------')
 	}
+	for (let i = 10; i > 0; i--) {
+		console.log(i)
+	}
+	// Can do this too which decrements i right before code block executes.
+	for (let i = 11; i > 0; ) {
+		i--
+		console.log(i)
+	}
 	console.log(battleLog)
 }
 
