ea80a889d7ba8e7eb5d7a2f5e0282b23aef3da8a
Matthew Shelbourn <mattshelbourn@gmail.com>
2020-01-18 15:17:18 -0800

    Granting system privs

--- oracleDB12cSQL/databases/sys.sql
+++ oracleDB12cSQL/databases/sys.sql
@@ -1,6 +1,58 @@
--- System Privaleges & Privaleges Practice 1
+-- System Privileges & Privileges Practice 1
 
 -- Connect to SYS database as SYSDBA on the ORCL container DB database
 
 -- 1.) You have to connect as SYSDBA
 -- 2.) When the DBA connects, it will be on the CONTAINER DB (ORCL)
+-- So the user should move to the pluggable database first
+
+show con_name;
+
+-- Move to the ORCL pluggable DB
+alter session set container = orclpdb;
+
+show con_name;
+
+-- The DBA can view all the users available from the ALL_USERS dictionary table
+select * from all_users;
+
+-- Use this query to view the system privileges map
+select * from system_privilege_map;
+
+-- Now let's create a new user called DEMO
+
+-- This query will create user DEMO with password "demo1234"
+-- This user will not be able to login until they are granted session privileges
+create user demo identified by demo1234;
+
+-- The following queries grant SYSTEM privileges to user DEMO
+-- You can also do this GRANT CREATE SESSION, CREATE TABLE TO DEMO
+grant create session to demo;
+
+grant create table to demo;
+
+-- DBA must allocate table size to users
+grant unlimited tablespace to demo;
+
+grant create sequence to demo;
+
+grant create view to demo;
+
+grant create synonym to demo;
+--------------------------------------------
+
+-- The following queries grant OBJECT privileges to DEMO
+grant select on hr.employees to demo;
+
+grant delete on hr.employees to demo;
+
+grant update (salary) on hr.employees to demo;
+
+grant all on hr.locations to demo;
+
+grant select, insert
+on hr.jobs to demo;
+
+grant select
+on hr.countries
+to public;
