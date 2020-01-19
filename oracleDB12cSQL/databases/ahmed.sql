181366c08ee0f269f558b37b6ba630b5d744e8e2
Matthew Shelbourn <mattshelbourn@gmail.com>
2020-01-18 16:48:27 -0800

    WITH GRANT option practice

--- oracleDB12cSQL/databases/ahmed.sql
+++ oracleDB12cSQL/databases/ahmed.sql
@@ -35,4 +35,42 @@ select * from demo.emp;
 update hr.employees
 set salary = salary+10
 where employee_id = 100;
+------------------------------------------------
 
+-- Using WITH GRANT Option
+
+--ahmed connection
+--1 create this table
+create table course
+( course_id number,
+  course_name varchar2(100)
+);
+
+--2
+
+-- HR can GRANT SELECT privalege on COURSE to other users
+grant select
+on course
+to hr
+with grant option;
+
+
+--3 go and open new session or connection to hr
+--do this : select * from ahmed.course
+--then this:
+/*
+grant select
+on ahmed.course
+to demo
+with grant option;
+*/
+
+--4 open new session or connection to demo
+--do this : select * from ahmed.course
+
+--5 now ahmed he will do this
+revoke select
+on course
+from hr;
+
+--6 then hr and also demo can not do this: select * from ahmed.course
