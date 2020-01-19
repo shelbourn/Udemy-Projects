60dde644fc31229f91c2b3d1ef26c1c5936e7b9f
Matthew Shelbourn <mattshelbourn@gmail.com>
2020-01-18 16:38:58 -0800

    Practice with creating ROLES

--- oracleDB12cSQL/databases/ahmed.sql
+++ oracleDB12cSQL/databases/ahmed.sql
@@ -2,11 +2,37 @@
 
 -- Testing privs granted to ROLES
 
--- Diplays SESSION privs
+-- Diplays SESSION privs (granted directly or through a ROLE)
+-- CREATE TABLE, CREATE VIEW, CREATE SEQUENCE, & SELECT ANY TABLE were granted via ROLEs
 select * from session_privs;
 
--- Displays granted SYSTEM privs
+-- Displays granted SYSTEM privs that were granted directly, not through a ROLE
 select * from user_sys_privs;
 
 -- Diplays granted ROLES
 select * from user_role_privs;
+
+-- Displays SYSTEM privs granted via ROLEs
+select * from role_sys_privs;
+
+-- Displays ROLE TABLE OBJECT privs granted
+select * from role_tab_privs
+where role = 'IUD_EMP';
+--------------------------------------
+
+create table student
+( student_id number,
+  student_name varchar2(100)
+);
+
+grant select on
+student to public;
+
+select * from hr.locations;
+
+select * from demo.emp;
+
+update hr.employees
+set salary = salary+10
+where employee_id = 100;
+
