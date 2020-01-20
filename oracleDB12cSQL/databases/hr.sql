95e8e369fb8a09b0c816e74b3d9813917dd036ed
Matthew Shelbourn <mattshelbourn@gmail.com>
2020-01-19 18:26:30 -0800

    More practice with MERGE statement

--- oracleDB12cSQL/databases/hr.sql
+++ oracleDB12cSQL/databases/hr.sql
@@ -6784,9 +6784,47 @@ order by 1; -- orders by column 1 (DEPARTMENT_ID) and then by column 2 (HIRE_DAT
 -- the MERGE statement
 -- Used when you want the TARGET table to be EXACTLY the same as the ORIGINAL table
 -- You MUST use table aliases when using the MERGE statement
+-- MERGE statement must be used on tables whose columns have the same data types
+-- MERGE can be substituted using separate INSERT and UPDATE statements
+-- but the MERGE statement performs both operations in 1 statement
 
 create table table_a
 ( id number,
   name varchar2(100)
 );
 
+select * from table_a;
+
+insert into table_a values (1, 'khaled');
+insert into table_a values (2, 'ali');
+insert into table_a values (3, 'ahmed');
+
+commit;
+
+select * from table_a;
+
+create table table_b
+( id number,
+  name varchar2(100)
+);  
+
+insert into table_b values (1, 'xxxxx');
+insert into table_b values (2, 'xxxxx');
+
+commit;
+
+select * from table_b;
+
+-- MERGE Statement
+merge into table_b b
+using (select * from table_a) a
+on (b.id = a.id)
+when matched then
+update
+set b.name = a.name
+when not matched then
+insert values (a.id, a.name);
+
+commit;
+
+select * from table_b;
