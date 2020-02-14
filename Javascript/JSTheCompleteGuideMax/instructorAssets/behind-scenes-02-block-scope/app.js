29e65c6bd3d3bcd40eb65eac764b3ef6591364ff
shelbourn <mattshelbourn@gmail.com>
2020-02-14 15:15:30 -0800

    Hoisting for Let, Const, Var

--- Javascript/JSTheCompleteGuideMax/instructorAssets/behind-scenes-02-block-scope/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/behind-scenes-02-block-scope/app.js
@@ -1,16 +1,24 @@
-let name = 'Matt'
+// let name = 'Matt'
 
-if (name === 'Matt') {
-	let hobbies = ['Music', 'Programming']
-	console.log(hobbies)
-}
+// if (name === 'Matt') {
+// 	let hobbies = ['Music', 'Programming']
+// 	console.log(hobbies)
+// }
 
-function greet() {
-	let age = 38
-	let name = 'Max'
-	console.log(name, age)
-}
+// function greet() {
+// 	let age = 38
+// 	let name = 'Max'
+// 	console.log(name, age)
+// }
 
-console.log(name, hobbies)
+// console.log(name, hobbies)
 
-greet()
+// greet()
+
+console.log(userName)
+
+var userName = 'Matt' // Returns 'Undefined'
+let userName = 'Matt' // Returns an error
+
+// Vars get hoisted with a value of 'Undefined'
+// Let and Consts get hoisted with no value assigned
