da35e59e9d1097700b1c2654345f4556e9907b26
shelbourn <mattshelbourn@gmail.com>
2020-02-14 15:37:19 -0800

    Strict mode

--- Javascript/JSTheCompleteGuideMax/instructorAssets/behind-scenes-03-strict-mode/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/behind-scenes-03-strict-mode/app.js
@@ -14,3 +14,12 @@
 // console.log(name, hobbies)
 
 // greet()
+
+// *** Strict mode ***
+
+'use strict' // Enables Strict Mode
+
+// This will be disabled with strict mode
+userName = 'Matt' // With Var, you can declare a variable without even using the 'Var' keyword
+
+console.log(userName)
