8de2b6b1c2ec94b5d1bc67c60226658a8e9ca32a
shelbourn <mattshelbourn@gmail.com>
2020-02-14 15:09:17 -0800

    Setting up code for lesson

---
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/behind-scenes-02-block-scope/app.js
@@ -0,0 +1,16 @@
+let name = 'Matt'
+
+if (name === 'Matt') {
+	let hobbies = ['Music', 'Programming']
+	console.log(hobbies)
+}
+
+function greet() {
+	let age = 38
+	let name = 'Max'
+	console.log(name, age)
+}
+
+console.log(name, hobbies)
+
+greet()
