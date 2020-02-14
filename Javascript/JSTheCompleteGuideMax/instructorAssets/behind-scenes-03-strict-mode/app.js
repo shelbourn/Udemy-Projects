b664b59d38a98a0d83c6fb716a2a86babdd1e0f0
shelbourn <mattshelbourn@gmail.com>
2020-02-14 15:40:24 -0800

    Strict mode in action

--- Javascript/JSTheCompleteGuideMax/instructorAssets/behind-scenes-03-strict-mode/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/behind-scenes-03-strict-mode/app.js
@@ -16,10 +16,12 @@
 // greet()
 
 // *** Strict mode ***
+// ES6 syntax (Let, Const) already restricts certain features that were available in ES5 and earlier
 
 'use strict' // Enables Strict Mode
 
 // This will be disabled with strict mode
-userName = 'Matt' // With Var, you can declare a variable without even using the 'Var' keyword
+const userName = 'Matt' // With Var, you can declare a variable without even using the 'Var' keyword
+var undefined = 5 // Disabled with strict mode
 
 console.log(userName)
