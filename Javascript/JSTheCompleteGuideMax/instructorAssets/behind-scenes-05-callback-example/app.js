0d24d3283328b2dde8ee828da85741c5d4c6a8a8
Matthew Shelbourn <mattshelbourn@gmail.com>
2020-02-16 15:32:19 -0800

    Adding code to demonstrate garbage collection.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/behind-scenes-05-callback-example/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/behind-scenes-05-callback-example/app.js
@@ -1,14 +1,18 @@
-const addListenerBtn = document.getElementById('add-listener-btn');
-const clickableBtn = document.getElementById('clickable-btn');
-const messageInput = document.getElementById('click-message-input');
+const addListenerBtn = document.getElementById('add-listener-btn')
+const clickableBtn = document.getElementById('clickable-btn')
+const messageInput = document.getElementById('click-message-input')
+
+let person = { name: 'Max' }
+
+person = null
 
 function printMessage() {
-  const value = messageInput.value;
-  console.log(value || 'Clicked me!');
+	const value = messageInput.value
+	console.log(value || 'Clicked me!')
 }
 
 function addListener() {
-  clickableBtn.addEventListener('click', printMessage);
+	clickableBtn.addEventListener('click', printMessage)
 }
 
-addListenerBtn.addEventListener('click', addListener);
+addListenerBtn.addEventListener('click', addListener)
