9feba52f93e032e1ed64774514d632b975ca2484
shelbourn <mattshelbourn@gmail.com>
2020-02-17 13:30:18 -0800

    Adding default value and connecting function to event listener

--- Javascript/JSTheCompleteGuideMax/instructorAssets/functions-04-game-user-choice/functions-04-game-user-choice/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/functions-04-game-user-choice/functions-04-game-user-choice/app.js
@@ -3,6 +3,7 @@ const startGameBtn = document.getElementById('start-game-btn')
 const ROCK = 'ROCK'
 const PAPER = 'PAPER'
 const SCISSORS = 'SCISSORS'
+const DEFAULT_USER_CHOICE = ROCK
 
 const getPlayerChoice = function() {
 	const selection = prompt(
@@ -11,9 +12,12 @@ const getPlayerChoice = function() {
 	).toUpperCase()
 	if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
 		alert(`Invalid choice! We chose ${ROCK} for you!`)
+		return DEFAULT_USER_CHOICE
 	}
+	return selection
 }
 
 startGameBtn.addEventListener('click', function() {
 	console.log('Game is starting...')
+	const playerSelection = getPlayerChoice
 })
