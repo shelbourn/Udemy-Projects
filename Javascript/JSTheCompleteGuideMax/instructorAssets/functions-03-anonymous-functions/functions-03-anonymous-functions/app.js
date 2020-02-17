e143cbd1301cee732d8c0aea55f60a7fd46c16bb
Matthew Shelbourn <mattshelbourn@gmail.com>
2020-02-16 16:44:12 -0800

    Reverting code to follow lesson.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/functions-03-anonymous-functions/functions-03-anonymous-functions/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/functions-03-anonymous-functions/functions-03-anonymous-functions/app.js
@@ -6,15 +6,6 @@ const startGameBtn = document.getElementById('start-game-btn')
 // }
 
 // Another use case for anonymous functions
-// In this case the function would not be called immediately
 startGameBtn.addEventListener('click', function() {
 	console.log('Game is starting...')
 })
-
-// In this case the function would be called immediately
-startGameBtn.addEventListener(
-	'click',
-	(function() {
-		console.log('Game is starting...')
-	})() // Extra parens call function immediately
-)
