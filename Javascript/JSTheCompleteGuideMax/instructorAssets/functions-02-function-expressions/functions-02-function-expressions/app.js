f5e681335de17f646e25cce5b0476abde58d1af5
Matthew Shelbourn <mattshelbourn@gmail.com>
2020-02-16 16:14:11 -0800

    Prepping code for lesson

--- Javascript/JSTheCompleteGuideMax/instructorAssets/functions-02-function-expressions/functions-02-function-expressions/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/functions-02-function-expressions/functions-02-function-expressions/app.js
@@ -1,18 +1,7 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById('start-game-btn')
 
 const start = function() {
-  console.log('Game is starting...');
-};
+	console.log('Game is starting...')
+}
 
-// const person = {
-//   name: 'Max',
-//   greet: function greet() {
-//     console.log('Hello there!');
-//   }
-// };
-
-// person.greet();
-
-// console.dir(startGame);
-
-startGameBtn.addEventListener('click', start);
+startGameBtn.addEventListener('click', start)
