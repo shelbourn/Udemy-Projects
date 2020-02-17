5cc040b8cc894a11073eb927410a748ea41eb1d9
Matthew Shelbourn <mattshelbourn@gmail.com>
2020-02-16 16:39:28 -0800

    Function Expressions vs Declarations

--- Javascript/JSTheCompleteGuideMax/instructorAssets/functions-03-anonymous-functions/functions-03-anonymous-functions/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/functions-03-anonymous-functions/functions-03-anonymous-functions/app.js
@@ -1,5 +1,8 @@
 const startGameBtn = document.getElementById('start-game-btn')
 
+// Function Declarations: Hoisted and initialized
+// Function Expressions: Hoisted and not initialized
+
 const start = function() {
 	console.log('Game is starting...')
 }
