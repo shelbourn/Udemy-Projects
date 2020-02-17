833a60f29e1a78c45fe6ac507aa78fb8cd0ccd57
Matthew Shelbourn <mattshelbourn@gmail.com>
2020-02-16 16:36:42 -0800

    Prepping code for lesson.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/functions-03-anonymous-functions/functions-03-anonymous-functions/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/functions-03-anonymous-functions/functions-03-anonymous-functions/app.js
@@ -1,20 +1,7 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById('start-game-btn')
 
-// const start = function() {
-//   console.log('Game is starting...');
-// };
+const start = function() {
+	console.log('Game is starting...')
+}
 
-// const person = {
-//   name: 'Max',
-//   greet: function greet() {
-//     console.log('Hello there!');
-//   }
-// };
-
-// person.greet();
-
-// console.dir(startGame);
-
-startGameBtn.addEventListener('click', function() {
-  console.log('Game is starting...');
-});
+startGameBtn.addEventListener('click', start)
