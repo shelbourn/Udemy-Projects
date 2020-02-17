f90652b341df6ff1bf4799226d55765608301f08
Matthew Shelbourn <mattshelbourn@gmail.com>
2020-02-16 16:07:43 -0800

    Storing functions in objects

--- Javascript/JSTheCompleteGuideMax/instructorAssets/functions-01-starting-code/functions-01-starting-code/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/functions-01-starting-code/functions-01-starting-code/app.js
@@ -4,6 +4,13 @@ function startGame() {
 	console.log('Game is starting...')
 }
 
+// Functions can be stored in objects
+const person = {
+	greet: function greet() {
+		console.log('Hello there!')
+	},
+}
+
 // startGame() // Direct Function execution
 
 startGameBtn.addEventListener('click', startGame) // Indirect function execution
