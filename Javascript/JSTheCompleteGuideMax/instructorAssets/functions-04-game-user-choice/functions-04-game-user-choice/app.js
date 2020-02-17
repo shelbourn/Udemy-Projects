c813f88b58c0013d7b605b49b67c50c74c05be37
shelbourn <mattshelbourn@gmail.com>
2020-02-17 13:26:59 -0800

    Adding more functionality

--- Javascript/JSTheCompleteGuideMax/instructorAssets/functions-04-game-user-choice/functions-04-game-user-choice/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/functions-04-game-user-choice/functions-04-game-user-choice/app.js
@@ -1,12 +1,12 @@
 const startGameBtn = document.getElementById('start-game-btn')
 
+const ROCK = 'ROCK'
+const PAPER = 'PAPER'
+const SCISSORS = 'SCISSORS'
+
 const getPlayerChoice = function() {
-	const selection = prompt('Rock, Paper or Scissors?', '')
-	if (
-		selection !== 'Rock' &&
-		selection !== 'Paper' &&
-		selection !== 'Scissors'
-	) {
+	const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase
+	if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
 		alert('Invalid choice! We chose Rock for you!')
 	}
 }
