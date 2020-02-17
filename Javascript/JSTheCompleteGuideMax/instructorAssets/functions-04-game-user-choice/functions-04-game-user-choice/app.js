9bbf4ed99b2a331cd641836e50009044d87720cf
shelbourn <mattshelbourn@gmail.com>
2020-02-17 13:02:22 -0800

    Adding first prompts of game

--- Javascript/JSTheCompleteGuideMax/instructorAssets/functions-04-game-user-choice/functions-04-game-user-choice/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/functions-04-game-user-choice/functions-04-game-user-choice/app.js
@@ -1,5 +1,16 @@
 const startGameBtn = document.getElementById('start-game-btn')
 
+const getPlayerChoice = function() {
+	const selection = prompt('Rock, Paper or Scissors?', '')
+	if (
+		selection !== 'Rock' &&
+		selection !== 'Paper' &&
+		selection !== 'Scissors'
+	) {
+		alert('Invalid choice! We chose Rock for you!')
+	}
+}
+
 startGameBtn.addEventListener('click', function() {
 	console.log('Game is starting...')
 })
