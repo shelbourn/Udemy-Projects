300c3e514bb6bf9e2e5bb9a4e87751e2e2795e80
Matthew Shelbourn <mattshelbourn@gmail.com>
2020-02-16 16:21:15 -0800

    Prepping code for lesson.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/functions-02-function-expressions/functions-02-function-expressions/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/functions-02-function-expressions/functions-02-function-expressions/app.js
@@ -4,7 +4,4 @@ function startGame() {
 	console.log('Game is starting...')
 }
 
-// Functions are objects and console.dir will show the key/value pairs
-console.dir(startGame) // .dir is similar to .log but provides more detail
-
 startGameBtn.addEventListener('click', startGame)
