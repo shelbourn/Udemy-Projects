26294be5721b45bde9704ada4742fc9a5c9e57a4
shelbourn <mattshelbourn@gmail.com>
2020-02-17 13:33:52 -0800

    Adding global variable to prevent multiple games from starting

--- Javascript/JSTheCompleteGuideMax/instructorAssets/functions-04-game-user-choice/functions-04-game-user-choice/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/functions-04-game-user-choice/functions-04-game-user-choice/app.js
@@ -5,6 +5,8 @@ const PAPER = 'PAPER'
 const SCISSORS = 'SCISSORS'
 const DEFAULT_USER_CHOICE = ROCK
 
+let gameIsRunning = false
+
 const getPlayerChoice = function() {
 	const selection = prompt(
 		`${ROCK}, ${PAPER} or ${SCISSORS}?`,
@@ -19,5 +21,6 @@ const getPlayerChoice = function() {
 
 startGameBtn.addEventListener('click', function() {
 	console.log('Game is starting...')
-	const playerSelection = getPlayerChoice
+	const playerSelection = getPlayerChoice() //Remember to use () at the end of a function's name
+	console.log(playerSelection)
 })
