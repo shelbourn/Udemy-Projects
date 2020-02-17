a7a2f24a7d2061a4c271a968a4a488b4fa7b95d7
Matthew Shelbourn <mattshelbourn@gmail.com>
2020-02-16 16:48:43 -0800

    Naming anonymous functions for debugging.

--- Javascript/JSTheCompleteGuideMax/instructorAssets/functions-03-anonymous-functions/functions-03-anonymous-functions/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/functions-03-anonymous-functions/functions-03-anonymous-functions/app.js
@@ -8,6 +8,7 @@ const startGameBtn = document.getElementById('start-game-btn')
 // Another use case for anonymous functions
 // This is useful if you will not need a function anywhere else in your code
 // Assigning anonymous functions to a const or variable may be useful to troubleshoot bugs
+// You can also name an anonymous function, which is helpful for debugging
 startGameBtn.addEventListener('click', function() {
 	console.log('Game is starting...')
 })
