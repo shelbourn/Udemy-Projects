5f018fa6aead445b1908fa7de71ac94002659e2a
Matthew Shelbourn <mattshelbourn@gmail.com>
2020-02-16 16:09:37 -0800

    Accessing functions within objects

--- Javascript/JSTheCompleteGuideMax/instructorAssets/functions-01-starting-code/functions-01-starting-code/app.js
+++ Javascript/JSTheCompleteGuideMax/instructorAssets/functions-01-starting-code/functions-01-starting-code/app.js
@@ -11,6 +11,8 @@ const person = {
 	},
 }
 
+person.greet() // Directly calls the greet() function within the person object as if you were calling a normal key/value pair
+
 // startGame() // Direct Function execution
 
 startGameBtn.addEventListener('click', startGame) // Indirect function execution
