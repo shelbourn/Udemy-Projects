3c9071a299bdd0cf6e51454890fa0427e17ca075
Matthew Shelbourn <mattshelbourn@gmail.com>
2020-03-18 13:36:21 -0700

    Testing .innerHTML to change HTML element content

--- Javascript/JSCorePractice/jsCorePractice.js
+++ Javascript/JSCorePractice/jsCorePractice.js
@@ -17,5 +17,12 @@
 
 /******* .innerHTML - Retrieves or modifies the contents of an HTML element */
 
-// Retrieving HTML of element
-let retrieveHTML = document.getElementById('Test').innerHTML
+// Retrieving HTML of element - assigns value of HTML element with ID "RetrieveTest" to retrieveHTML
+let retrieveHTML = document.getElementById('RetrieveTest').innerHTML
+
+console.log(retrieveHTML)
+
+// Changing HTML of element - modified the content of HTML element with ID "ChangeTest"
+
+document.getElementById('ChangeTest').innerHTML =
+	'If you are seeing this then .innerHTML has successfully modified the content of this HTML element'
