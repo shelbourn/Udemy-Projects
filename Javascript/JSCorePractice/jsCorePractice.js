713e9491722ac12b1a9d9e6651c8b1b54b95d6eb
Matthew Shelbourn <mattshelbourn@gmail.com>
2020-03-18 13:14:54 -0700

    Building out test html page

---
+++ Javascript/JSCorePractice/jsCorePractice.js
@@ -0,0 +1,18 @@
+// JS METHODS
+
+/**************************
+ * List to be Practiced
+ *
+ * for each...in
+ * .map()
+ * .filter()
+ * .reduce()
+ * .split()
+ * setTimeout()
+ * clearTimeout()
+ * setInterval()
+ * clearInterval()
+ * .innerHTML
+ */
+
+// .innerHTML - Retrieves or modifies the contents of an HTML element
